7208f4e8976663023b58b2983aed638584d7e016
nikhilganireddy <nikhilganireddy@yahoo.com>
2024-03-19 17:45:13 +0530

    Commit 9

--- app/page.tsx
+++ app/page.tsx
@@ -15,20 +15,22 @@ const Page = () => {
     // @ts-ignore
     const {User, setUser} = useGlobalUserContext()
 
-    const creatingUser = async ()=> {
-        // @ts-ignore
-        await createUser(user.user?.id)
-    }
-    creatingUser().then(()=>toast("User Updated"))
-
-    // @ts-ignore
-     findUser({userId: user.user?.id}).then((result) => {
-        if (result !== null) {
-            if (result.userName !== User.userName && result.role !== User.role) {
-                setUser({userName: result.userName, role: result.role,})
-            }
+    if (user.isSignedIn){
+        const creatingUser = async ()=> {
+            // @ts-ignore
+            await createUser(user.user?.id)
         }
-    })
+        creatingUser().then(()=>toast("User Updated"))
+
+        // @ts-ignore
+        findUser({userId: user.user?.id}).then((result) => {
+            if (result !== null) {
+                if (result.userName !== User.userName && result.role !== User.role) {
+                    setUser({userName: result.userName, role: result.role,})
+                }
+            }
+        })
+    }
 
     return <main className={`w-full flex min-h-screen flex-col justify-center items-center gap-8`}>
         <h1 className={`text-4xl`}>KSM - Placement Portal</h1>
